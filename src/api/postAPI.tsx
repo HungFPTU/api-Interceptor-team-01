3b204551ef0faaefda79ab168f911cbe6c4e634e
HungFPTU <nghung179123@gmail.com>
2024-09-25 15:38:42 +0700

    add image to post

--- src/api/postAPI.tsx
+++ src/api/postAPI.tsx
@@ -3,17 +3,16 @@ import apiClient from './apiClient';
 const getAllPostAPI = () => {
     return apiClient.get("/post");
 }
-
 const getPostIdAPI = ( ({ postId }: { postId: number}) => {
     return apiClient.get(`/post/${postId}`);
 })
-const updatePostIdAPI = ( ({postId} : {postId : number} ,{title, dateCreate, content, userId }: {title : string, dateCreate : number, content : string, userId : number}) => {
-    return apiClient.put(`/post/${postId}`, { title, dateCreate, content, userId });
+const updatePostIdAPI = ( ({postId} : {postId : number} ,{title, dateCreate, content, userId, image }: {title : string, dateCreate : number, content : string, userId : number, image : string}) => {
+    return apiClient.put(`/post/${postId}`, { title, dateCreate, content, userId, image });
 })
 const deletetePostIdAPI = ( ( {postId} : {postId : number}) => {
     return apiClient.delete(`/post/${postId}` );
 })
-const createNewPostAPI = ( ({ title, dateCreate, content, userId }: { title : string, dateCreate : number, content : string, userId : number}) => {
-    return apiClient.post(`/post/`, { title, dateCreate, content, userId });
+const createNewPostAPI = ( ({ title, dateCreate, content, userId, image }: { title : string, dateCreate : number, content : string, userId : number, image : string}) => {
+    return apiClient.post(`/post/`, { title, dateCreate, content, userId, image });
 })
 export default {getAllPostAPI, getPostIdAPI, updatePostIdAPI, deletetePostIdAPI, createNewPostAPI};
