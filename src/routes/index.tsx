d38c974ae3ac222c91298aefb9af9d7d52e48b89
doit-now <baohg1204@gmail.com>
2024-09-28 16:41:32 +0700

    managerHB

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,24 @@ import {DefaultLayout} from '../layout'
 import { RoleEnum, RouteConfig } from "../model/RouteConfig";
 import UpdateUser from "../pages/UpdateUser";
 import Register from "../pages/Register";
+import ManagerPost from "../pages/ManagerPost";
 
-const publicRoute : RouteConfig[] = [
-    { path: '/', component: Home, layout: DefaultLayout, role : RoleEnum.Guest },
-    { path: '/login', component: Login, layout : null, role : RoleEnum.Guest },
-    { path: '/updateuser', component: UpdateUser, layout : null, role : RoleEnum.User },
-    { path: "register", component: Register, layout: null, role: RoleEnum.Guest },
-    
+const publicRoute: RouteConfig[] = [
+  { path: "/", component: Home, layout: DefaultLayout, role: RoleEnum.Guest },
+  { path: "/login", component: Login, layout: null, role: RoleEnum.Guest },
+  {
+    path: "/updateuser",
+    component: UpdateUser,
+    layout: null,
+    role: RoleEnum.User,
+  },
+  { path: "register", component: Register, layout: null, role: RoleEnum.Guest },
+  {
+    path: "/manager-post",
+    component: ManagerPost,
+    layout: null,
+    role: RoleEnum.User,
+  },
 ];
 
 const privateRoute :  RouteConfig[] = [
